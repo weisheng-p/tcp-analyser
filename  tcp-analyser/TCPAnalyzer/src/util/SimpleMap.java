8eff6cccf0d48ff249f6b3bc926046261d217bc1
pws1906@gmail.com <pws1906@gmail.com@fdfcfde4-d9de-5adf-93f0-cce6a5e376d0>
2011-10-28 13:57:30 +0000

    fixed a bug where stray packets are not removed from the active connection

---  tcp-analyser/TCPAnalyzer/src/util/SimpleMap.java
+++  tcp-analyser/TCPAnalyzer/src/util/SimpleMap.java
@@ -1,5 +1,7 @@
 package util;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Set;
 
 /**
  * SimpleMap is a very crude Map implementation.
@@ -8,7 +10,7 @@ import java.util.ArrayList;
  * @param <K> the type of keys maintained by this map
  * @param <V> the type of mapped values
  */
-public class SimpleMap<K,V> {
+public class SimpleMap<K,V> { //implements Map<K, V>{
 	private ArrayList<K> keys;
 	private ArrayList<V> values; 
 	
@@ -36,7 +38,7 @@ public class SimpleMap<K,V> {
 	 * @param key the key whose associated value is to be returned.
 	 * @return the value to which this map maps the specified key, or null if the map contains no mapping for this key.
 	 */
-	public V get(K key)
+	public V get(Object key)
 	{
 		int i = keys.indexOf(key);
 		if(i == -1)
@@ -47,7 +49,7 @@ public class SimpleMap<K,V> {
 	}
 	
 	/**
-	 *  Associates the specified value with the specified key in this map. If the map previously contained a mapping for this key, the old value is replaced.
+	 * Associates the specified value with the specified key in this map. If the map previously contained a mapping for this key, the old value is replaced.
 	 * @param key key with which the specified value is to be associated.
 	 * @param value value to be associated with the specified key.
 	 * @return previous value associated with specified key, or null if there was no mapping for key.
@@ -70,9 +72,10 @@ public class SimpleMap<K,V> {
 	 * @param key The key whose presence in this map is to be tested
 	 * @return <tt>true</tt> if this map contains a mapping for the specified key.
 	 */
-	public boolean containsKey(K key)
+	public boolean containsKey(Object key)
 	{
 		return keys.contains(key);
+	
 	}
 	
 	/**
@@ -80,7 +83,7 @@ public class SimpleMap<K,V> {
 	 * @param key key whose mapping is to be removed from the map.
 	 * @return previous value associated with specified key, or null if there was no mapping for key.
 	 */
-	public V remove(K key)
+	public V remove(Object key)
 	{
 		int i = keys.indexOf(key);
 		if(i == -1) return null;
@@ -95,5 +98,42 @@ public class SimpleMap<K,V> {
 	{
 		values.clear();
 		keys.clear();
+	}
+
+//	@Override
+//	public boolean containsValue(Object value) {
+//		// TODO Auto-generated method stub
+//		return values.contains(value);
+//	}
+//
+//	@Override
+//	public Set<java.util.Map.Entry<K, V>> entrySet() {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	@Override
+//	public boolean isEmpty() {
+//		// TODO Auto-generated method stub
+//		return keys.size() == 0;
+//	}
+//
+//	@Override
+	public Set<K> keySet() {
+		// TODO Auto-generated method stub
+		return new java.util.HashSet<K>(keys);
+	}
+//
+//	@Override
+//	public void putAll(Map<? extends K, ? extends V> m) {
+////		for(Map.Entry<? extends K, ? extends V> e: m.entrySet())
+////		{
+////			put(e.getKey(),e.getValue());
+////		}
+//	}
+//
+//	@Override
+	public Collection<V> values() {
+		return values;
 	}
 }
