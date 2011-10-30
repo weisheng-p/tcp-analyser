0243b90af5d447c2455eb9d5fcc746d947e19c23
pws1906@gmail.com <pws1906@gmail.com@fdfcfde4-d9de-5adf-93f0-cce6a5e376d0>
2011-10-30 05:02:32 +0000

    accounted for RTT on both side

--- trunk/ tcp-analyser/TCPAnalyzer/src/TCP/Flow.java
+++ trunk/ tcp-analyser/TCPAnalyzer/src/TCP/Flow.java
@@ -13,13 +13,52 @@ public class Flow {
 	public int srcPort, destPort;
 	public String srcIP, destIP;
 	public int num_dupAck = 0,
-			   num_outOfOrder = 0;	// number of out of order packet
+			   num_outOfOrder = 0;
 	public SlidingWindow 	srcWindow = new SlidingWindow(), 
-					destWindow = new SlidingWindow();
+							destWindow = new SlidingWindow();
 	public long dataLength = 0;
-	public State current = State.INIT;
-	long started = 0;
+	public State current = State.INIT, prev = State.INIT;
 	boolean predicited = false;
+	public static final float RTT_ALPHA = 0.9f;
+	
+	// estimated
+	public float incomingRTT = 0, outgoingRTT = 0;
+	public long	lastSend = 0, lastRecv = 0;
+	public Direction lastDirection = Direction.INCOMING; 
+	
+	public void updateRTT (PacketInfo pi)
+	{
+		//estimatedRtt = Flow.RTT_ALPHA * estimatedRtt + (1 - Flow.RTT_ALPHA) * sampledRTT;
+		if(pi.direction.equals(Direction.INCOMING))
+		{
+			if(lastDirection.equals(Direction.INCOMING))
+			{
+				lastRecv = pi.time;	// reset timer
+			}
+			else
+			{
+				// update rtt
+				outgoingRTT = Flow.RTT_ALPHA * outgoingRTT + (1 - Flow.RTT_ALPHA) * (pi.time - lastSend);
+			}
+			lastRecv = pi.time;
+		}
+		else
+		{
+			if(lastDirection.equals(Direction.OUTGOING))
+			{
+				lastSend = pi.time;	// reset timer
+			}
+			else
+			{
+				// update rtt
+				incomingRTT = Flow.RTT_ALPHA * incomingRTT + (1 - Flow.RTT_ALPHA) * (pi.time - lastRecv);
+			}
+			lastSend = pi.time;
+		}
+		
+		lastDirection = pi.direction;
+	}
+	
 	public Flow(int srcPort, int destPort, String srcIP, String destIP) {
 		this.srcPort = srcPort;
 		this.destPort = destPort;
@@ -39,8 +78,9 @@ public class Flow {
 	}
 	void checkDirection(PacketInfo pi)
 	{
-		if(destIP.equals(pi.destIP) && srcIP.equals(pi.srcIP)) pi.incoming = false;
-		else pi.incoming = true;
+		if(destIP.equals(pi.destIP) && srcIP.equals(pi.srcIP))
+			pi.direction = Direction.INCOMING;
+		else pi.direction = Direction.OUTGOING;
 	}
 	
 	public void predictState(PacketInfo pi)
@@ -80,16 +120,19 @@ public class Flow {
 
 	private void addToSlidingWindow(PacketInfo pi) {
 		if(pi.dataLen == 0) return;
-		if(pi.incoming)
+		if(pi.direction.equals(Direction.INCOMING))
 		{
 			if(srcWindow.getNextExpectedSeqNum() != pi.seqNum && srcWindow.started) num_outOfOrder++;
 			srcWindow.addFilledWindow(pi.seqNum, pi.seqNum + pi.dataLen);
+			srcWindow.updateWindowSize(pi.window);
 		}
 		else
 		{
 			if(destWindow.getNextExpectedSeqNum() != pi.seqNum && srcWindow.started) num_outOfOrder++;
 			destWindow.addFilledWindow(pi.seqNum, pi.seqNum + pi.dataLen);
+			destWindow.updateWindowSize(pi.window);
 		}
+		updateRTT(pi);
 	}
 	
 	/**
@@ -187,5 +230,10 @@ public class Flow {
 	public enum State
 	{
 		INIT, SYNC, SYNC_ACK, ACK, DATA_TRANSFER, FIN, FIN_ACK, TERMINIATED, STRAY;
+	}
+	
+	public enum Direction
+	{
+		INCOMING, OUTGOING;
 	}
 }
